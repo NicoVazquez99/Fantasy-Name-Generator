f0b4dafa0cf784d7d2e1a4068e3ab95c2ff65173
NicoVazquez99 <136937287+NicoVazquez99@users.noreply.github.com>
2023-06-19 21:10:13 -0300

    new changes
    added icons, footer, contact information, changed the "descrition" and the title

--- Fonts/DancingScript/RandomNameGenerator-master/cumulative_roundoff.rb
+++
@@ -1,7 +0,0 @@
-module CumulativeRoundoff
-private
-  def roundoff(value)
-    return 1.0 if value < 1.0 && value  > 0.998
-    value
-  end
-end
--- Fonts/DancingScript/RandomNameGenerator-master/random_name_generator_test.rb
+++
@@ -1,6 +0,0 @@
-require_relative 'random_name_generator'
-
-generator = RandomNameGenerator.new("media/greek_myth_sample")
-
-puts "Generating 40 names"
-40.times {puts generator.generate}
