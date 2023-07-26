f2f02e87d7a03ea7eb112148f25ab1d4c62f3d16
NicoVazquez99 <136937287+NicoVazquez99@users.noreply.github.com>
2023-07-25 23:13:17 -0300

    shakling error solved

--- Javascript/navMenu.js
+++ Javascript/navMenu.js
@@ -1,22 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var menuItems = document.querySelectorAll("nav > ul > li"); // Seleccionar solo los elementos del menú principal
+    var homeButton = document.querySelector("nav > ul > li > a"); // Seleccionar el botón "Home"
+    var submenu = document.querySelector("nav ul.submenu");
+    var pageContent = document.getElementById("page-content");
 
-    menuItems.forEach(function(menuItem) {
-        var submenu = menuItem.querySelector("ul");
-        var pageContent = document.getElementById("page-content");
+    homeButton.addEventListener("click", function(event) {
+        event.preventDefault(); // Evitar el comportamiento predeterminado del enlace
 
-        menuItem.addEventListener("mouseover", function() {
-            if (submenu) {
-                submenu.style.display = "block";
-                pageContent.style.marginTop = submenu.offsetHeight + "px";
-            }
-        });
-
-        menuItem.addEventListener("mouseout", function() {
-            if (submenu) {
-                submenu.style.display = "none";
-                pageContent.style.marginTop = "0";
-            }
-        });
+        if (submenu.style.display === "block") {
+            submenu.style.display = "none";
+            pageContent.style.marginTop = "0";
+        } else {
+            submenu.style.display = "block";
+            pageContent.style.marginTop = submenu.offsetHeight + "px";
+        }
     });
 });
